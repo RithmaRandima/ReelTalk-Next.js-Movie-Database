7b5fd6705c4569dcf3d20ff5cdd82ae755efc36c
RithmaRandima <rithma.r.nandasiri@gmail.com>
2026-04-23 10:06:23 +0530

    feat:implementing about page

---
+++ app/about/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-3">
+      <h1 className="text-2xl font-medium text-cyan-500">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nam
+        at sit a tempore accusamus ducimus, nisi est exercitationem aliquam
+        cumque quis repellendus neque minima ipsa consectetur eveniet
+        consequatur officiis non quia. Vero saepe molestias rem numquam
+        doloribus tenetur dolore debitis repellendus soluta? Animi dolorem
+        sapiente est quidem ea iure cum placeat hic, consequuntur aspernatur
+        doloremque deserunt earum eum reprehenderit, laboriosam error at qui,
+        cumque quis corporis. Aliquam voluptas fugit error magni expedita labore
+        commodi ipsa ratione, officiis beatae dolore similique sed quaerat eos
+        temporibus aliquid, suscipit accusamus laboriosam cumque dolorem nobis
+        tempore minus! Aspernatur expedita aliquam inventore quibusdam
+        voluptate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        atque ipsam obcaecati! Inventore, voluptatibus repudiandae. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Corporis hic eligendi eaque
+        temporibus cupiditate eveniet iusto, ducimus autem aspernatur facilis
+        placeat, laboriosam dolor tempore, iste qui aperiam repellendus quae
+        ipsa. Quibusdam pariatur molestiae fuga perspiciatis, ea repellendus
+        deleniti ad molestias consequatur iure odio nam neque possimus odit quia
+        illum consequuntur minus maiores! Ipsa sunt repellendus consequuntur
+        officia veniam beatae officiis atque consectetur exercitationem.
+        Adipisci quo officia iusto non explicabo quidem soluta quos ipsum nobis
+        nesciunt vero, eaque exercitationem, eveniet iure culpa. Eum id laborum
+        temporibus reiciendis ullam, animi molestias obcaecati nulla pariatur,
+        illum, ea dolores dolore ratione deserunt! Similique illum quos officiis
+        quasi ratione, dolores distinctio? Sequi asperiores iusto unde
+        aspernatur cupiditate totam quia velit voluptas quisquam. Est culpa
+        repellendus quaerat nam nulla. Porro molestiae ducimus nostrum aliquam
+        ipsum autem accusamus cumque architecto, repellat deserunt dolorem quis!
+        Alias nulla perspiciatis, consectetur, architecto quo iure commodi quos
+        repudiandae accusantium quas exercitationem?
+      </p>
+    </div>
+  );
+};
+
+export default About;
